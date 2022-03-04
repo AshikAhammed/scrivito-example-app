d71e243c60ed3892d11e55850ed202c4139fa5f0
Md Ashik Ahammed <asa@click-solutions.de>
2022-03-04 10:50:26 +0100

    initial commit

---
+++ src/Widgets/ColumnContainerWidget/ColumnContainerWidgetEditingConfig.js
@@ -0,0 +1,26 @@
+import loadable from "@loadable/component";
+import * as Scrivito from "scrivito";
+import columnContainerWidgetIcon from "../../assets/images/column_container_widget.svg";
+import ColumnWidget from "../ColumnWidget/ColumnWidgetClass";
+
+Scrivito.provideEditingConfig("ColumnContainerWidget", {
+  title: "Columns",
+  thumbnail: columnContainerWidgetIcon,
+  propertiesGroups: [
+    {
+      title: "Columns layout",
+      component: loadable(() =>
+        import("../../Components/ScrivitoExtensions/ColumnsEditorTab")
+      ),
+      key: "columns-layout-group",
+    },
+  ],
+  initialContent: {
+    columns: [
+      new ColumnWidget({ colSize: 4 }),
+      new ColumnWidget({ colSize: 4 }),
+      new ColumnWidget({ colSize: 4 }),
+    ],
+    alignment: "start",
+  },
+});
