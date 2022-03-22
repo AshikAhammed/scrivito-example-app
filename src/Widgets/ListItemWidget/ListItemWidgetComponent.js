2fb988becaa05aef1a242db978a6d9fc79369f74
Md Ashik Ahammed <asa@click-solutions.de>
2022-03-22 11:31:25 +0100

    feat: list widget

--- src/Widgets/ListItemWidget/ListItemWidgetComponent.js
+++ src/Widgets/ListItemWidget/ListItemWidgetComponent.js
@@ -2,10 +2,12 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 
 Scrivito.provideComponent("ListItemWidget", ({ widget }) => {
-  <Scrivito.WidgetTag
-    tag="li"
-    className={widget.get("cssClass") || "list-group-item"}
-  >
-    <Scrivito.ContentTag content={widget} attribute="content" />
-  </Scrivito.WidgetTag>;
+  return (
+    <Scrivito.WidgetTag
+      tag="li"
+      className={widget.get("cssClass") || "list-group-item"}
+    >
+      <Scrivito.ContentTag content={widget} attribute="content" />
+    </Scrivito.WidgetTag>
+  );
 });
--- src/Widgets/ListWidget/ListWidgetComponent.js
+++ src/Widgets/ListWidget/ListWidgetComponent.js
@@ -2,12 +2,12 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 
 Scrivito.provideComponent('ListWidget', ({widget}) =>{
+   return(
     <Scrivito.ContentTag
-    
     tag='ul'
     attribute="items"
     content={widget}
     className= { widget.get('cssClass') || 'list-group'}    
-    
     />
+   )
 })
